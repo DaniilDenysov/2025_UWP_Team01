55aa74750de939c70fbd9e3250537c67dea7b2fd
Oleksandr-Marych <112111959+AlexMarych@users.noreply.github.com>
2025-03-26 14:09:44 +0100

    SoundManager Added

---
+++ Assets/Scripts/AudioSystem/SoundManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    private GameObject audioSourceItem;
+    private AudioMixerGroup audioMixers;
+
+    private void Start()
+    {
+             
+    }
+
+    private void OoClipPlay()
+    {
+        
+    }
+
+    private void PlayBackgroudMusic(AudioClip clip)
+    {
+
+    }
+}
+
